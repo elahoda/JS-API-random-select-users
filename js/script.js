716475f34c1932865ce876a5dedf44a504cd7e22
Eric Lahoda <elahoda@gmail.com>
2023-04-03 14:37:22 -0400

    API-with-user-selection

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const selectUserNumber =document.querySelector("#users");
+const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function(numUsers){
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
+    const data = await usersRequest.json();
+    console.log(data);
+    const userResults =data.results;
+ // console.log(userResults); 
+    displayUsers(userResults);
+};
+
+getData(1);
+
+const displayUsers = function(userResults){
+    randomFolks.innerHTML = "";
+    for (let user of userResults){
+        country = user.location.country;               
+        name= user.name.first;
+        imageUrl=user.picture.medium;
+
+
+    const userDiv = document.createElement("div"); 
+    userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="User avatar" />
+    `;
+    randomFolks.append(userDiv);
+}
+};
+
+selectUserNumber.addEventListener("change", function(e){
+    const numUsers = e.target.value;
+    console.log(numUsers);
+    getData(numUsers);
+});
